742b51733b7de7b7fb1a02cabd20d5af55c3f7de
Aiden Madaffri <Aiden.Madaffri@gmail.com>
2018-12-23 18:59:03 -0600

    Initial Commit

---
+++ AMExpenses.Qml.Net/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AMExpenses.Core.Entities;
+using AMExpenses.Core.Database;
+
+namespace AMExpenses.Qml.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
